bf8d13d87585be9182a568f7103c2607d28d0a53
Jordanafri <139744212+Jordanafri@users.noreply.github.com>
2024-05-31 12:44:47 +0200

    PushOrigin

---
+++ EcclesiaWeb/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EcclesiaWeb.Data;
+using EcclesiaWeb.Models;
+
+namespace EcclesiaWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            // goes to db andfest and makes list
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+
+
+        // ---Actions
+
+        //create
+    }
+}
